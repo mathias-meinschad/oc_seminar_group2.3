2b17f4118218e2e0fa5650dd6f6c2dd802ed677f
Mottl94 <Mathias.Meinschad@student.uibk.ac.at>
2020-05-25 16:03:57 +0200

    trying fixing it

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-console.log('Defining port');
+var express = require('express');
+var app = express();
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+var port = process.env.PORT || 8080;
 
-console.log('Port defined');
+app.set('view engine')
+
+// set the home page route
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
