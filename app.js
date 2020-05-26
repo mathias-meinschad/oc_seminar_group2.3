d1c6ca4b09c9b1efe28e7fb1be248521edb068bb
Hanna Köb <hanna.koeb@student.uibk.ac.at>
2020-05-26 12:02:00 +0200

    connecting to repository

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 
+
+
 app.get('/', (req, res) => {
     const {ServerClient, ServerClientConfig} = require('graphdb').server;
     const {RDFMimeType} = require('graphdb').http;
     const {RepositoryClientConfig} = require('graphdb').repository;
+    const {SparqlJsonResultParser} = require('graphdb').parser;
+    const {GetQueryPayload, QueryType} = require('graphdb').query;
     
     const serverConfig = new ServerClientConfig('http://graphdb.sti2.at:8080/', 0, {
         'Accept': RDFMimeType.SPARQL_RESULTS_JSON
@@ -18,39 +22,44 @@ app.get('/', (req, res) => {
         if (exists) {
             // repository exists -> delete it for example
             console.log('DB exits')
-        }
+
+            const readTimeout = 30000;
+            const writeTimeout = 30000;
+            const repositoryClientConfig = new RepositoryClientConfig(['http://graphdb.sti2.at:8080/repositories/OCSS2020'], {}, '', readTimeout, writeTimeout);
+            server.getRepository('OCSS2020',repositoryClientConfig).then(function(repository) {  
+
+                // console.log('REPO:', repository);
+                repository.registerParser(new SparqlJsonResultParser());
+
+                const payload = new GetQueryPayload()
+                  .setQuery('select * where {?s ?p ?o}')
+                  .setQueryType(QueryType.SELECT)
+                  .setResponseType(RDFMimeType.SPARQL_RESULTS_XML)
+                  .setLimit(100);
+
+                return repository.query(payload);
+
+           }).then(function(stream) {
+            stream.on('data', (bindings) => {
+                // the bindings stream converted to data objects with the registered parser
+              });
+              stream.on('end', () => {
+                // handle end of the stream
+              });
+            })
+            .catch(function(error) {
+                console.log('ERROR', error);
+            });
+
+            }
     }).catch(err => console.log(err));
-
-    const readTimeout = 30000;
-    const writeTimeout = 30000;
-    const repositoryClientConfig = new RepositoryClientConfig(['http://graphdb.sti2.at:8080/'], {}, '', readTimeout, writeTimeout);
-    const repo = server.getRepository('OCSS2020', repositoryClientConfig).then((rdfRepositoryClient) => {
-        // rdfRepositoryClient is a configured instance of RDFRepositoryClient
-    });
-
-      
-    const payload = new GetQueryPayload()
-        .setQuery('ask { ?person schema:description ?name . } limit 100 ')
-        .setQueryType(QueryType.ASK)
-        .setResponseType(RDFMimeType.BOOLEAN_RESULT);
- 
-    repo.registerParser(new SparqlJsonResultParser());
-    
-    repo.query(payload).then((data) => {
-        if (data) {
-            console.log("its true");
-        } else {
-            console.log("its false");
-        }
-    });
-
 
     res.status(200).send('Server is working.')
 })
 
-app.listen(port, () => {
-	console.log(`🌏 Server is running at https://intelligent-textbook.herokuapp.com:${port}`)
-})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 app.post('/testApp', (req, res) => {
     console.log('Received call from google assistant');
