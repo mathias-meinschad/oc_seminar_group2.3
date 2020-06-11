471de62390c49595d3eec3ee5a70c3fd8edbe0cb
Mottl94 <Mathias.Meinschad@student.uibk.ac.at>
2020-06-11 13:14:58 +0200

    added error handling and intent classes to the webhook
    * first working version with http request
    
    * Added mehmets stuff to my branch
    
    * fixed /get
    
    * added logging response
    
    * test
    
    * added logging
    
    * added more logging
    
    * test
    
    * fix
    
    * better readability of the logs
    
    * more testing
    
    * fixed query
    
    * specified log in more detail
    
    * trying new  stuff
    
    * Differentiation of intents
    
    * added logging
    
    * added logging
    
    * added error management
    
    * error fix
    
    * fixingn errors
    
    * more logging
    
    * actually i hate java script :D
    
    * more logging
    
    * rollback
    
    * trying stuff
    
    * added logging
    
    * js is really fucking dogshit
    
    * added logging
    
    * async problems
    
    * added logging
    
    * i think i finally got it
    
    * added switch instead of if
    
    * its fucking working
    
    * refactoring
    
    * refactoring

--- app.js
+++ app.js
@@ -3,72 +3,81 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+const axios = require('axios');
 
+const host_name = "https://graphdb.sti2.at/repositories/OCSS2020?";
+
+const authenticationParams = {
+	auth: {
+		username: 'oc1920',
+		password: 'Oc1920!'
+	}
+}
+
+const querystring = require('querystring');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-    const {ServerClient, ServerClientConfig} = require('graphdb').server;
-    const {RDFMimeType} = require('graphdb').http;
-    const {RepositoryClientConfig} = require('graphdb').repository;
-    const {SparqlJsonResultParser} = require('graphdb').parser;
-    const {GetQueryPayload, QueryType} = require('graphdb').query;
-    
-    const serverConfig = new ServerClientConfig('http://graphdb.sti2.at:8080/', 0, {
-        'Accept': RDFMimeType.SPARQL_RESULTS_JSON
-    });
-    const server = new ServerClient(serverConfig);
-
-    server.hasRepository('OCSS2020').then(exists => {
-        if (exists) {
-            // repository exists -> delete it for example
-            console.log('DB exits')
-
-            const readTimeout = 30000;
-            const writeTimeout = 30000;
-            const repositoryClientConfig = new RepositoryClientConfig(['http://graphdb.sti2.at:8080/repositories/OCSS2020'], {}, '', readTimeout, writeTimeout);
-            server.getRepository('OCSS2020',repositoryClientConfig).then(function(repository) {  
-
-                // console.log('REPO:', repository);
-                repository.registerParser(new SparqlJsonResultParser());
-
-                const payload = new GetQueryPayload()
-                  .setQuery('select * where {?s ?p ?o}')
-                  .setQueryType(QueryType.SELECT)
-                  .setResponseType(RDFMimeType.SPARQL_RESULTS_XML)
-                  .setLimit(100);
-
-                return repository.query(payload);
-
-           }).then(function(stream) {
-            stream.on('data', (bindings) => {
-                // the bindings stream converted to data objects with the registered parser
-              });
-              stream.on('end', () => {
-                // handle end of the stream
-              });
-            })
-            .catch(function(error) {
-                console.log('ERROR', error);
-            });
-
-            }
-    }).catch(err => console.log(err));
-
-    res.status(200).send('Server is working.')
+    res.status(200).send("Server is running")
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port, () => {
+	console.log(`🌏 Server is running at https://intelligent-textbook.herokuapp.com:${port}`)
+})
 
 app.post('/testApp', (req, res) => {
-    console.log('Received call from google assistant');
-    
-    console.log(req.body);
-    
-    console.log('Returning smth to google assistant');
-    return res.json({
-        fulfillmentText: 'Check that out, this response is from heroku',
-        source: 'getmovie'
-      })
+	try {
+		console.log("Intent is: " + req.body.queryResult.intent.displayName)
+
+		switch (req.body.queryResult.intent.displayName) {
+			case "What is Type Question": 
+				return callGraphDb(req, res)
+			default: {
+					return res.json({
+						fulfillmentText: 'Webhook Error: Intent could not be parsed.',
+						source: 'testApp'
+					})
+				}
+		}
+	} catch (e) {
+		console.log(e)
+		return res.json({
+			fulfillmentText: 'Webhook Error: ' + e,
+			source: 'testApp'
+		})
+	}
 })
+
+function callGraphDb(req, res) {
+	var requested_intent = req.body.queryResult.parameters.placeholder_generated_entities;
+
+	var encoded_query = querystring.stringify({query: `
+			PREFIX schema: <http://schema.org/>
+
+			select * where { 
+				?Concept schema:name ?name.
+				OPTIONAL {?Concept schema:purpose ?purpose.}
+				OPTIONAl {?Concept schema:description ?description.}
+				filter contains(LCASE(?name), LCASE("${requested_intent}"))
+			}
+		`
+		});	// pre-defined query sample.. needs to be improved to handle complicated queries -> only returns purpose or description for the passed 'name'..
+	
+	let url = host_name + encoded_query
+	
+	axios.get(url,authenticationParams).then(response =>{			
+		let response_value = (typeof response.data.results.bindings[0].purpose === 'undefined') ? response.data.results.bindings[0].description.value 
+		: response.data.results.bindings[0].purpose.value;	// checks out if the return type is 'purpose' or 'description' and set the value for fulfilmment text..
+
+		return res.json({
+			fulfillmentText: response_value,
+			source: 'testApp'
+		});
+	}).catch(error => {
+		console.log(error);
+		res.send(error);
+	});
+}
