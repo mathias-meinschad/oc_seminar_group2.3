b1661eb5d4ccf6d27bdbbf7e2876e9e11aa5d9eb
Mottl94 <Mathias.Meinschad@student.uibk.ac.at>
2020-06-11 13:04:13 +0200

    added switch instead of if

--- app.js
+++ app.js
@@ -32,47 +32,45 @@ app.post('/testApp', (req, res) => {
 	try {
 		console.log("Intent is: " + req.body.queryResult.intent.displayName)
 
-		if (req.body.queryResult.intent.displayName == "What is Type Question") {
-			console.log("inside if\n")
+		switch (req.body.queryResult.intent.displayName) {
+			case "What is Type Question": {
+					var requested_intent = req.body.queryResult.parameters.placeholder_generated_entities;
 
-			var requested_intent = req.body.queryResult.parameters.placeholder_generated_entities;
-
-			var encoded_query = querystring.stringify({query: `
-					PREFIX schema: <http://schema.org/>
-
-					select * where { 
-						?Concept schema:name ?name.
-						OPTIONAL {?Concept schema:purpose ?purpose.}
-						OPTIONAl {?Concept schema:description ?description.}
-						filter contains(LCASE(?name), LCASE("${requested_intent}"))
-					}
-				`
-				});	// pre-defined query sample.. needs to be improved to handle complicated queries -> only returns purpose or description for the passed 'name'..
-			
-			let url = host_name + encoded_query
-			
-			axios.get(url,authenticationParams).then(response =>{			
-				let response_value = (typeof response.data.results.bindings[0].purpose === 'undefined') ? response.data.results.bindings[0].description.value 
-				: response.data.results.bindings[0].purpose.value;	// checks out if the return type is 'purpose' or 'description' and set the value for fulfilmment text..
-				
-				console.log("response value is: " + response_value)
-
-				return res.json({
-					fulfillmentText: response_value,
-					source: 'testApp'
-				});
-			}).catch(error => {
-				console.log(error);
-				res.send(error);
-			});
-		} else {
-			return res.json({
-				fulfillText: 'Intent could not be parsed.',
-				source: 'testApp'
-			})
-		}
-
+					var encoded_query = querystring.stringify({query: `
+							PREFIX schema: <http://schema.org/>
 		
+							select * where { 
+								?Concept schema:name ?name.
+								OPTIONAL {?Concept schema:purpose ?purpose.}
+								OPTIONAl {?Concept schema:description ?description.}
+								filter contains(LCASE(?name), LCASE("${requested_intent}"))
+							}
+						`
+						});	// pre-defined query sample.. needs to be improved to handle complicated queries -> only returns purpose or description for the passed 'name'..
+					
+					let url = host_name + encoded_query
+					
+					axios.get(url,authenticationParams).then(response =>{			
+						let response_value = (typeof response.data.results.bindings[0].purpose === 'undefined') ? response.data.results.bindings[0].description.value 
+						: response.data.results.bindings[0].purpose.value;	// checks out if the return type is 'purpose' or 'description' and set the value for fulfilmment text..
+		
+						return res.json({
+							fulfillmentText: response_value,
+							source: 'testApp'
+						});
+					}).catch(error => {
+						console.log(error);
+						res.send(error);
+					});
+				}
+				break
+			default: {
+					return res.json({
+						fulfillText: 'Intent could not be parsed by Webhook',
+						source: 'testApp'
+					})
+				}
+		}
 	} catch (e) {
 		console.log(e)
 		return res.json({
