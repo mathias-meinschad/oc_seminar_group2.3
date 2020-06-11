9d86925707c6c5bfbbca33572c5af4482c33dfe7
Mottl94 <Mathias.Meinschad@student.uibk.ac.at>
2020-06-11 13:09:28 +0200

    refactoring

--- app.js
+++ app.js
@@ -33,40 +33,11 @@ app.post('/testApp', (req, res) => {
 		console.log("Intent is: " + req.body.queryResult.intent.displayName)
 
 		switch (req.body.queryResult.intent.displayName) {
-			case "What is Type Question": {
-					var requested_intent = req.body.queryResult.parameters.placeholder_generated_entities;
-
-					var encoded_query = querystring.stringify({query: `
-							PREFIX schema: <http://schema.org/>
-		
-							select * where { 
-								?Concept schema:name ?name.
-								OPTIONAL {?Concept schema:purpose ?purpose.}
-								OPTIONAl {?Concept schema:description ?description.}
-								filter contains(LCASE(?name), LCASE("${requested_intent}"))
-							}
-						`
-						});	// pre-defined query sample.. needs to be improved to handle complicated queries -> only returns purpose or description for the passed 'name'..
-					
-					let url = host_name + encoded_query
-					
-					axios.get(url,authenticationParams).then(response =>{			
-						let response_value = (typeof response.data.results.bindings[0].purpose === 'undefined') ? response.data.results.bindings[0].description.value 
-						: response.data.results.bindings[0].purpose.value;	// checks out if the return type is 'purpose' or 'description' and set the value for fulfilmment text..
-		
-						return res.json({
-							fulfillmentText: response_value,
-							source: 'testApp'
-						});
-					}).catch(error => {
-						console.log(error);
-						res.send(error);
-					});
-				}
-				break
+			case "What is Type Question": 
+				return callGraphDb()
 			default: {
 					return res.json({
-						fulfillmentText: 'Intent could not be parsed by Webhook',
+						fulfillmentText: 'Webhook Error: Intent could not be parsed.',
 						source: 'testApp'
 					})
 				}
@@ -74,8 +45,39 @@ app.post('/testApp', (req, res) => {
 	} catch (e) {
 		console.log(e)
 		return res.json({
-			fulfillmentText: 'Error in webhook: ' + e,
+			fulfillmentText: 'Webhook Error: ' + e,
 			source: 'testApp'
 		})
 	}
 })
+
+function callGraphDb() {
+	var requested_intent = req.body.queryResult.parameters.placeholder_generated_entities;
+
+	var encoded_query = querystring.stringify({query: `
+			PREFIX schema: <http://schema.org/>
+
+			select * where { 
+				?Concept schema:name ?name.
+				OPTIONAL {?Concept schema:purpose ?purpose.}
+				OPTIONAl {?Concept schema:description ?description.}
+				filter contains(LCASE(?name), LCASE("${requested_intent}"))
+			}
+		`
+		});	// pre-defined query sample.. needs to be improved to handle complicated queries -> only returns purpose or description for the passed 'name'..
+	
+	let url = host_name + encoded_query
+	
+	axios.get(url,authenticationParams).then(response =>{			
+		let response_value = (typeof response.data.results.bindings[0].purpose === 'undefined') ? response.data.results.bindings[0].description.value 
+		: response.data.results.bindings[0].purpose.value;	// checks out if the return type is 'purpose' or 'description' and set the value for fulfilmment text..
+
+		return res.json({
+			fulfillmentText: response_value,
+			source: 'testApp'
+		});
+	}).catch(error => {
+		console.log(error);
+		res.send(error);
+	});
+}
